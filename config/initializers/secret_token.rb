39688cd3b4371d67afc0cb357622ce4395c62d38
Philip Crawford <philip.crawford@uwalumni.com>
2011-12-19 20:57:34 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seedly::Application.config.secret_token = '7585a00ddb2d5bf83abefe9877825198d8e2bccc228ac4536796b6bf50023f320320a3aa619dedd39bb01fb0f1c38f030a40281d050baf146730bc6264ef39ee'
