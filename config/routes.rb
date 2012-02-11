0efdefc79348c10416b22b8b0902e2a5b9c0aa0f
Philip Crawford <philip.crawford@uwalumni.com>
2012-02-11 12:47:35 -0600

    how works and home buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Seedly::Application.routes.draw do
   root :to => 'content#index'
-  match "/choose_kit" => "content#choose_kit"
-  match "/about" => "content#about"
-  match "/how_this_works" => "content#how_this_works"
+  match "/choose_kit" => "content#choose_kit", :as => "choose_kit"
+  match "/about" => "content#about", :as => "about"
+  match "/how_this_works" => "content#how_this_works", :as => "how_works"
 end
