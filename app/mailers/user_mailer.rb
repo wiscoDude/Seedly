d1c4161d1b904358e0357e1e0eddaaa58373cc8b
Philip Crawford <philip.crawford@uwalumni.com>
2012-03-17 15:38:17 -0500

    user mailer start

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   
-  def needs_waterhing(user)
+  def needs_watering(user)
     setup_email(user)
     @recipients = "philip@seedly.me"
-    @subject << "#{user.name} needs to consider"
+    @subject << "#{user.name} needs to consider watering"
   end
   
 protected
