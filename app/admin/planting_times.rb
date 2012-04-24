963727e8b509cac82f76e0d45ae0736c830fe2ec
Philip Crawford <philip.crawford@uwalumni.com>
2012-04-24 09:53:11 -0500

    Display plantint times in plant show

--- app/admin/planting_times.rb
+++ app/admin/planting_times.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register PlantingTime do
+  index do
+    column :growing_zone, sortable: 'planting_time.growing_zone.name'
+    column :plant, sortable: 'planting_time.plant.name'
+    
+    default_actions
+  end
   
 end
--- app/admin/plants.rb
+++ app/admin/plants.rb
@@ -8,4 +8,21 @@ ActiveAdmin.register Plant do
     
     default_actions
   end
+  show do
+    attributes_table *default_attribute_table_rows
+    
+    panel "Planting Times" do 
+      table_for plant.planting_times do |planting_time|
+        column :growing_zone, sortable: 'planting_time.growing_zone.name'
+        column :earliest_spring_inside
+        column :earliest_spring_outside
+        column :latest_spring_inside
+        column :latest_spring_outside
+        column :earliest_fall_inside
+        column :earliest_fall_outside
+        column :latest_fall_inside
+        column :latest_fall_outside
+      end
+    end 
+  end
 end
