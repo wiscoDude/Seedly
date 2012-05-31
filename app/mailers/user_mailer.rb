b30d6223b3dca1b2b2f59bf23db0dfd3c78e315d
Philip Crawford <philip.crawford@uwalumni.com>
2012-05-31 10:36:27 -0500

    Welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,16 @@
 class UserMailer < ActionMailer::Base
   
+  def welcome_email(user)
+    setup_email(user)
+    @recipients = user.email
+    @bcc = "info@seedly.me"
+    @subject = "Welcome to Seedly"
+  end
+  
   def needs_watering(user)
     setup_email(user)
-    @recipients = "philip@seedly.me"
-    @subject << "#{user.name} needs to consider watering"
+    @recipients = user.email
+    @subject = "#{user.name} needs to consider watering"
   end
   
 protected
