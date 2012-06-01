06a5641aa64884a0d0d02777806a795fcbd32fe7
Philip Crawford <philip.crawford@uwalumni.com>
2012-06-01 08:54:34 -0500

    fuck rails email sending

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,17 @@
 class UserMailer < ActionMailer::Base
-  default :content_type => 'text/html'
+  default :content_type => 'text/html',
+    :from => 'Seedly <info@seedly.me>',
+    :bcc => 'bcc dude <philip@seedly.me>'
+    
   
   def welcome(user)
-    setup_email(user)
     @recipients = user.email
-    @bcc = "info@seedly.me"
     @subject = "Welcome to Seedly"
   end
   
   def needs_watering(user)
-    setup_email(user)
     @recipients = user.email
     @subject = "#{user.name} needs to consider watering"
   end
   
-protected
-  def setup_email(user)
-    @from = "info@seedly.me"
-    @sent_on = Time.now
-  end
 end
